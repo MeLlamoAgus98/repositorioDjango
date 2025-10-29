26e56b4e52a09b302d0ec55fd729d2c4979c78cd
i210 <gaby210404gmail.com>
2025-10-28 21:20:14 -0300

    commit fallido

--- Cosas/Aplicacion/models.py
+++ Cosas/Aplicacion/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db import date
+
 
 # Create your models here.
+class producto (models.models):
+    nombre =models.CharField(max_lenght=40)
+    precio =models.CharField(max_lenght=15)
+    descripcion =models.CharField(max_lenght=130)
+    fecha =models.DateField()
+    cantidad =models.CharField(max_lenght=5)
+
+def __str__(self):
+   return self.nombre
