0a1029732dd9338727a52f02ce96c6d78d4f0c79
MeLlamoAgus98 <agusriple@hotmail.com>
2025-11-04 14:27:59 -0300

    Template basico

--- Cosas/Aplicacion/models.py
+++ Cosas/Aplicacion/models.py
@@ -1,14 +1,13 @@
 from django.db import models
-from django.db import date
+from datetime import date
 
 
-# Create your models here.
-class producto (models.models):
-    nombre =models.CharField(max_lenght=40)
-    precio =models.CharField(max_lenght=15)
-    descripcion =models.CharField(max_lenght=130)
-    fecha =models.DateField()
-    cantidad =models.CharField(max_lenght=5)
+class Producto(models.Model):  
+    nombre = models.CharField(max_length=40)       
+    precio = models.CharField(max_length=15)
+    descripcion = models.CharField(max_length=130)
+    fecha = models.DateField(default=date.today)   
+    cantidad = models.CharField(max_length=5)
 
-def __str__(self):
-   return self.nombre
+    def __str__(self):
+        return self.nombre
--- Cosas/CosasProyecto/asgi.py
+++ Cosas/CosasProyecto/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cosas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CosasProyecto.settings')
 
 application = get_asgi_application()
